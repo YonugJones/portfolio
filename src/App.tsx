3ade0939af08ce47bb91ca4cac565c03e5b732a1
Peter Kerfoot <peterckerfoot@gmail.com>
2025-07-27 15:18:22 -0500

    created file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import About from './sections/About'
+import Contact from './sections/Contact'
+import Hero from './sections/Hero'
+import Projects from './sections/Projects'
+import Skills from './sections/Skills'
+
 export default function App() {
   return (
-    <>
-      <h1>APP</h1>
-    </>
+    <div className='h-screen'>
+      <NavBar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
