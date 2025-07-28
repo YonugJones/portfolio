a7abcf1628219fd35073c5311e1b42c1900cd466
Peter Kerfoot <peterckerfoot@gmail.com>
2025-07-28 10:17:00 -0500

    added skills to dataset

---
+++ src/data/skillList.ts
@@ -0,0 +1,32 @@
+import { type Skill } from '../types/skillType'
+import js from '../assets/js.png'
+import ts from '../assets/ts.png'
+import reactIcon from '../assets/react.png'
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import nodejs from '../assets/nodejs.png'
+import restapi from '../assets/rest-api.png'
+import tailwind from '../assets/tailwindcss.png'
+import figma from '../assets/figma.png'
+import git from '../assets/git.png'
+import jest from '../assets/jest.png'
+import postgresql from '../assets/postgresql.png'
+import uiux from '../assets/ui-ux.png'
+import ps from '../assets/ps.png'
+
+export const skillList: Skill[] = [
+  { name: 'JavaScript', icon: js },
+  { name: 'TypeScript', icon: ts },
+  { name: 'React', icon: reactIcon },
+  { name: 'HTML', icon: html },
+  { name: 'CSS', icon: css },
+  { name: 'NodeJS', icon: nodejs },
+  { name: 'Rest API', icon: restapi },
+  { name: 'TailwindCSS', icon: tailwind },
+  { name: 'Figma', icon: figma },
+  { name: 'Git', icon: git },
+  { name: 'Jest', icon: jest },
+  { name: 'PostgreSQL', icon: postgresql },
+  { name: 'UI / UX', icon: uiux },
+  { name: 'Photoshop', icon: ps },
+]
