037a358c385be5065a1eaf54fe96ab05c5b0e809
Peter Kerfoot <peterckerfoot@gmail.com>
2025-07-28 09:22:50 -0500

    added about me section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,14 +1,31 @@
 const About = () => {
   return (
-    <div>
+    <div className='py-10'>
+      <h1 className='text-3xl text-center pb-2'>About Me</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        <span className='font-medium text-[var(--dark-three)]'>I began</span> as
+        a general manager at a bakery where I worked with patrons to design
+        cakes for their special events. I learned how to balance the needs of
+        the client with a budget and deliver an experience that left everyone
+        happy. I worked with wholesale clients with large event and wedding
+        orders and learned how to perform under pressure.
+      </p>
+      <br />
+      <p>
+        <span className='font-medium text-[var(--dark-three)]'>I am</span>{' '}
+        currently working on finishing a meta front end certification program.
+        I've created various front end and full stack applications to highlight
+        my abilities behind the console. I'm also working on steadily working on
+        projects for small companies around me
+      </p>
+      <br />
+      <p>
+        <span className='font-medium text-[var(--dark-three)]'>I hope to</span>{' '}
+        finish creating a user database meets social media app for people living
+        with hemophilia, a stressful and at times debilitating disease which
+        affects the life of about 1 in 5000 people worldwide. I'd like to create
+        useful features and environemtns for people who are not able to be
+        mobile and may feel isolated
       </p>
     </div>
   )
