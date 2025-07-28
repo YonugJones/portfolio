73971c5b0ae1980d6c9a905bf534c20331b80e10
Peter Kerfoot <peterckerfoot@gmail.com>
2025-07-28 09:23:23 -0500

    added and styled hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,34 @@
+import heroImage from '../assets/hero-image.png'
+
 const Hero = () => {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <div className='py-10'>
+      <div className='grid grid-cols-1 md:grid-cols-[1fr_300px] gap-6 items-center'>
+        <div>
+          <h2 className='text-lg text-[var(--dark-three)]'>
+            Hello! My name is
+          </h2>
+          <h1 className='text-4xl font-medium'>Peter 'PK' Kerfoot</h1>
+          <h2 className='text-xl mt-2 font-medium text-[var(--dark-four)]'>
+            Web Developer • UX/UI Designer
+          </h2>
+          <p className='mt-4'>
+            I am a front-end web developer fascinated with feelings,
+            connectivity, and textures. Based in Chicago, my background in
+            managing a custom bakery and designing special event cakes taught me
+            the power of storytelling through theme and design. This, combined
+            with a love of math and problem-solving, lead me to software
+            development where I can now focus that energy on user experience
+          </p>
+        </div>
+        <div className='w-[300px] h-[300px] mx-auto'>
+          <img
+            src={heroImage}
+            alt='Picture of Peter Kerfoot'
+            className='w-full h-full object-cover rounded-full'
+          />
+        </div>
+      </div>
     </div>
   )
 }
