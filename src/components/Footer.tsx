b3813898badeaeb516f510c4ebde0c0c9a082463
Peter Kerfoot <peterckerfoot@gmail.com>
2025-07-28 09:51:37 -0500

    Created footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className='text-center py-4 text-sm text-[var(--dark-four)]'>
+      &copy; 2025 Peter Kerfoot
+    </footer>
+  )
 }
 
 export default Footer
