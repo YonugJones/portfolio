9080324fe664c38789e8b05c77b85d8643ed9a40
Peter Kerfoot <peterckerfoot@gmail.com>
2026-02-15 19:14:13 -0600

    feat: added social app project to projectList

--- src/data/projectList.ts
+++ src/data/projectList.ts
@@ -1,18 +1,29 @@
 import type { Project } from '../types'
-import chessAppPic from '../assets/chess-app-pic.png'
+import socialAppPic from '../assets/social-app-frame-pic.png'
 import imageGalleryPic from '../assets/image-gallery-pic.png'
 import worldNewsPic from '../assets/world-news-pic.png'
+import chessAppPic from '../assets/chess-app-pic.png'
 
 export const projectList: Project[] = [
   {
     id: 1,
-    imageUrl: chessAppPic,
-    name: 'Chess',
+    imageUrl: socialAppPic,
+    name: 'Social App',
     description:
-      'This is a playable game of chess, influenced by the UI of Chess.com and Lichess.org, two of the most popular apps online for playing chess. Features include a clean user interface, move previews, move history and move jumping, undo, and a winning delcaration',
-    tools: ['TypeScript ', 'JavaScript ', 'React ', 'Tailwind CSS'],
-    githubUrl: 'https://github.com/YonugJones/chess-app',
-    liveDemo: 'https://chess-app-five-phi.vercel.app/',
+      'This is the foundation of a social app website that can be used for future social application projects.',
+    tools: [
+      'NextJs ',
+      'Typescript ',
+      'Javascript ',
+      'React ',
+      'Tailwind CSS',
+      'PostgreSQL',
+      'Prisma ',
+      'Shadcn ',
+      'Lucide-React',
+    ],
+    githubUrl: 'https://github.com/YonugJones/social-app-frame',
+    liveDemo: 'https://social-app-frame.vercel.app/',
   },
   {
     id: 2,
@@ -33,5 +44,15 @@ export const projectList: Project[] = [
     tools: ['TypeScript ', 'JavaScript ', 'React ', 'TailwindCSS'],
     githubUrl: 'https://github.com/YonugJones/world-news',
     liveDemo: 'https://world-news-2obivokcp-pks-projects-7213db00.vercel.app/',
+  },
+  {
+    id: 4,
+    imageUrl: chessAppPic,
+    name: 'Chess',
+    description:
+      'This is a playable game of chess, influenced by the UI of Chess.com and Lichess.org, two of the most popular apps online for playing chess. Features include a clean user interface, move previews, move history and move jumping, undo, and a winning delcaration',
+    tools: ['TypeScript ', 'JavaScript ', 'React ', 'Tailwind CSS'],
+    githubUrl: 'https://github.com/YonugJones/chess-app',
+    liveDemo: 'https://chess-app-five-phi.vercel.app/',
   },
 ]
